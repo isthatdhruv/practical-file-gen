647e7b39578092f6d42cca3016c2596718256790
isthatdhruv <isthatdhruvv@gmail.com>
2025-06-09 10:43:13 +0530

    backend update

--- server/index.js
+++ server/index.js
@@ -9,7 +9,11 @@ const { PDFDocument } = require('pdf-lib');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+const allowedOrigins = [process.env.FRONTEND_URL || 'http://localhost:3000'];
+app.use(cors({
+  origin: allowedOrigins,
+}));
+
 app.use(express.json());
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
